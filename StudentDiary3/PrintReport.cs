6d54d70e1ae843a702cd9b1571ea1a467f3dfe38
Nerijus <Npeciulis@yahoo.com>
2022-04-20 21:59:43 +0300

    class Print all reports

--- StudentDiary3/PrintReport.cs
+++ StudentDiary3/PrintReport.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using StudentDiary3.DataAcces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,30 @@ namespace StudentDiary3
 {
     public class PrintReport
     {
+        public StudentDiaryReportRepo studentDiaryReportRepo { get; }
+        public StudentDiaryGenerate studentDiaryGenerate { get; }
+
+
+        public PrintReport()
+        {
+            studentDiaryReportRepo = new StudentDiaryReportRepo();
+            studentDiaryGenerate = new StudentDiaryGenerate();
+
+        }
+
+        public void PrintAllReports()
+        {
+            List<string> studentsDiaryList = studentDiaryGenerate.RetrieveDiaryFormated(studentDiaryReportRepo.Retrieve());
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Generated Students Diary reports:");
+            Console.WriteLine("------------------------------------");
+            Console.ForegroundColor = ConsoleColor.Green;
+            for (int i = 0; i < studentsDiaryList.Count; i++)
+            {
+                Console.WriteLine(studentsDiaryList[i]);
+            }
+        }
 
     }
 }
